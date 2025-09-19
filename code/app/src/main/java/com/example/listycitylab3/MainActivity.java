dde5e673316e45cdbe0aea3f440ae7b3882059a9
TexasNo99 <sktt998dasima@gmail.com>
2025-09-19 16:56:29 -0600

    Add files via upload

--- code/app/src/main/java/com/example/listycitylab3/MainActivity.java
+++ code/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -1,36 +1,49 @@
 package com.example.listycitylab3;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ArrayList<String> dataList;
+public class MainActivity extends AppCompatActivity implements
+        AddCityFragment.AddCityDialogListener {
+    private ArrayList<City> dataList;
     private ListView cityList;
-    private ArrayAdapter<String> cityAdapter;
+    private CityArrayAdapter cityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] cities = {
-                "Edmonton", "Vancouver", "Moscow",
-                "Sydney", "Berlin", "Vienna",
-                "Tokyo", "Beijing", "Osaka", "New Delhi"
-        };
+        String[] cities = { "Edmonton", "Vancouver", "Toronto" };
+        String[] provinces = { "AB", "BC", "ON" };
 
         dataList = new ArrayList<>();
-        dataList.addAll(Arrays.asList(cities));
-        
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
         cityList = findViewById(R.id.city_list);
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityAdapter = new CityArrayAdapter(this, dataList);
         cityList.setAdapter(cityAdapter);
+
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            City selectedCity = dataList.get(position);
+            AddCityFragment fragment = AddCityFragment.newInstance(selectedCity);
+            fragment.show(getSupportFragmentManager(), "Edit City");
+        });
+    }
+
+    // The following method is from OpenAI, ChatGPT,
+    // "How to editing the city?", 2025-09-19
+    @Override
+    public void addCity(City city) {
+        if (!dataList.contains(city)) {
+            dataList.add(city);
+        }
+        cityAdapter.notifyDataSetChanged();
     }
 }
